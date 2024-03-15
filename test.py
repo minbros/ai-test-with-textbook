e9b82e56de62721dae585e5af7e7bbb659677b3f
minbros <pristo24631@gmail.com>
2024-03-15 14:46:33 +0900

    코드 정리

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from deepl import Translator
 from pinecone import Pinecone
 from openai import OpenAI
 
-THRESHOLD = 0.75
+THRESHOLD = 0.8
 
 # 실행을 위해서는 환경 변수에 다음의 키가 등록되어 있어야 함
 deepl_api_key = os.getenv('DEEPL_API_KEY')
@@ -14,7 +14,7 @@ pinecone_api_key = os.getenv('PINECONE_API_KEY')
 # Pinecone, OpenAI와 Deepl API를 사용하기 위한 객체 생성
 client = OpenAI(api_key=openai_api_key)
 translator = Translator(auth_key=deepl_api_key)
-pc = Pinecone(api_key=pinecone_api_key)
+pc = Pinecone()
 index = pc.Index("miraen-test")
 
 
@@ -71,7 +71,7 @@ def read_file_and_process(filename):
 
 
 # 텍스트 데이터 불러오기
-sample_text = read_file_and_process("titles-of-textbook.txt")
+sample_text = read_file_and_process("sample-textbook.txt")
 
 # titles-of-textbook.txt에서 대단원, 소단원, 설명 추출
 title_pattern = r"^\d+\."
@@ -90,37 +90,65 @@ explains = [explain.lstrip('{').rstrip('}').strip() for explain in explains]
 english_titles = translate_texts(titles)
 english_subtitles = translate_texts(subtitles)
 
-print(subtitles)
-print(explains)
-print(english_titles)
-print(english_subtitles)
+# # Pinecone에 데이터를 upsert하기 위한 객체 생성
+# pc = Pinecone(api_key=pinecone_api_key)
+# index = pc.Index("miraen-test")
 
 # # Pinecone에 데이터 업로드
-# vectors_of_explains = []
+# vectors_of_explains = embed_texts(explains)
 #
-# for explain in explains:
-#     vectors_of_explains.append(explain)
-#
-# for subtitle, explain in zip(english_subtitles, vectors_of_explains):
+# for subtitle, vector in zip(english_subtitles, vectors_of_explains):
 #     index.upsert(
-#         vectors=[{"id": subtitle, "values": embed_text(explain)}]
+#         vectors=[{"id": subtitle, "values": vector}],
+#         namespace='test'
 #     )
 
-sample_explain = input("Enter the explaination: ")
-translated_explain = translate_text(sample_explain)
+# # Pinecone에 단원명과 유사도 검사를 위한 데이터 업로드
+# vectors_of_subtitles = embed_texts(english_subtitles)
+#
+# for subtitle, vector in zip(english_subtitles, vectors_of_subtitles):
+#     index.upsert(
+#         vectors=[{"id": subtitle, "values": vector}],
+#         namespace='subtitles'
+#     )
 
-# sample_explain을 영어로 번역한 후 Pinecone에 쿼리
-queried = index.query(
-    vector=embed_text(translated_explain),
-    top_k=1,
-)
+while True:
+    sample_explain = input("Enter the explaination: ")
 
-score = queried.matches[0].score
-matches = [item.id for item in queried.matches if item.score >= THRESHOLD]
+    # "종료" 입력 시 종료
+    if sample_explain == "종료":
+        break
 
-print("Translated explaination: " + translated_explain)
-if score < THRESHOLD:
-    print("No matched title")
-else:
-    print(f"Matched title: {matches[0]}")
-print(f"The score of the most similar title: {queried.matches[0].score}")
+    # sample_explain을 영어로 번역한 후 Pinecone에 쿼리
+    translated_explain = translate_text(sample_explain)
+
+    queried = index.query(
+        vector=embed_text(translated_explain),
+        top_k=1,
+        namespace='test'
+    )
+
+    # 단원명과 유사한지 체크하기 위한 쿼리
+    similarity_check = index.query(
+        vector=embed_text(translated_explain),
+        top_k=1,
+        namespace='subtitles'
+    )
+
+    queried_score = queried.matches[0].score
+    check_score = similarity_check.matches[0].score
+    matches = [item.id for item in queried.matches if item.score >= THRESHOLD]
+
+    print("Translated explaination: " + translated_explain)
+    print(f"The score of the most similar title: {queried_score}")
+    print(f"The score of the most similar name: {check_score}")
+
+    # 쿼리한 점수가 임계값보다 낮으면 일치하는 단원명이 없다고 출력
+    if queried_score < THRESHOLD:
+        print("No matched title")
+    # 쿼리한 점수가 임계값보다 높지만 단원명과 유사도가 높으면 설명이 단원명과 유사하다고 출력
+    elif check_score > 0.9:
+        print("The explaination is too similar with the subtitle name.")
+    # 그 외의 경우에는 일치하는 단원명 출력
+    else:
+        print(f"Matched title: {matches}")
